ae802f2eb901fdaa40f9c28133774458db8d8bca
Ryu CAPISTRANO <e185753@ie.u-ryukyu.ac.jp>
2019-01-15 12:03:47 +0900

    Step4 I tried to make Miner class but I couldn't... I made mineBlock method and isChainValid method.

--- src/main/java/jp/ac/uryukyu/ie/e185753/blockchain.java
+++ src/main/java/jp/ac/uryukyu/ie/e185753/blockchain.java
@@ -1,16 +1,43 @@
 package jp.ac.uryukyu.ie.e185753;
-import java.util.Scanner;
+import java.util.ArrayList;
+import com.google.gson.*;
 
-public class blockchain {
-    public static void main(String[] args){
-        Block genesisblock = new Block(dataUserInput(),"0");// genesis block has no previous block so I put "0" as previousHash
-        System.out.println("this is genesis block`s hash : "+genesisblock.hash);
+public class Blockchain {
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficultyLevel = 6;
+    public static void main(String[] args){//dataUserInput()
 
+        blockchain.add(new Block(Block.setDataUserInput(),"0"));
+        blockchain.add(new Block(Block.setDataUserInput(),blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block(Block.setDataUserInput(),blockchain.get(blockchain.size()-1).hash));
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        //Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        //String blockchainJson = gson.toJson(blockchain);
+        System.out.println(blockchainJson);
     }
-    public static String dataUserInput(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("This is dataUserInput:");
-        String Str_data = sc.nextLine();
-        return Str_data;
+    public static boolean isChainValid(){
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficultyLevel]).replace('\0', '0');
+
+        for(int i = 0; i < blockchain.size(); i ++){
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            //compare registered hash and calculated hash
+            if(!currentBlock.hash.equals(currentBlock.makeHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            if(!currentBlock.hash.substring( 0, difficultyLevel).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+        return true;
     }
 }
+
